2705969d102d19d9bb1951119c32e6cedcafddb8
ehab qino <ehabqino@gmail.com>
2021-07-28 12:01:31 +0300

    commit "promise"

--- promise.js
+++ promise.js
@@ -1,6 +1,22 @@
 
 let myPromise = new Promise((resolve,reject) => {
+
+    setTimeout(()=>{
+       // resolve("Hello from Ehab");
+        reject("Errrrrrrrrrrr error")
+    },1000);
     
 });
 
-console.log(myPromise);
+//console.log(myPromise);
+
+myPromise.then((res)=>{
+    console.log(res);
+    console.log(myPromise);
+},
+(err)=>{
+    console.log(err);
+    console.log(myPromise);
+}
+);
+
