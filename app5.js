679f050f9f2520dddfb8b009e3088061b53baa12
ehab qino <ehabqino@gmail.com>
2021-07-28 10:50:41 +0300

    commit "sync ,async,callback"

--- app5.js
+++ app5.js
@@ -15,8 +15,14 @@ class CardComponent extends HTMLElement {
         
         //check if the tag has attributes
         if(this.hasAttribute("heading-color")){
-            this.shadowRoot.querySelector(".heading").getElementsByClassName.color = this.getAttribute('heading-color');
+            this.shadowRoot.querySelector(".heading").style.color = this.getAttribute("heading-color");
+        }
+        
+        if(this.hasAttribute('bg-color')){
+            this.shadowRoot.querySelector(".card-container").style.backgroundColor = this.getAttribute("bg-color");
         }
     }
 }
 customElements.define('my-card',CardComponent);
+
+
