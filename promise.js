d361bcf805d49c8928f98a5214f6cbb7efcbc45f
ehab qino <ehabqino@gmail.com>
2021-07-28 12:17:00 +0300

    commit "user promise then and catch"

--- promise.js
+++ promise.js
@@ -1,8 +1,8 @@
-
+/*
 let myPromise = new Promise((resolve,reject) => {
 
     setTimeout(()=>{
-       // resolve("Hello from Ehab");
+        //resolve("Hello from Ehab");
         reject("Errrrrrrrrrrr error")
     },1000);
     
@@ -13,10 +13,29 @@ let myPromise = new Promise((resolve,reject) => {
 myPromise.then((res)=>{
     console.log(res);
     console.log(myPromise);
-},
-(err)=>{
-    console.log(err);
-    console.log(myPromise);
-}
-);
+}).catch((err)=>{           ///.catch as same as myPromise.catch , this is a chain method
+    console.log("Error",err);
+});
 
+// myPromise.catch((err)=> {
+//     console.log("Error",err);
+// });
+*/
+
+function myPromise(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(10);
+            //reject(500);
+        },1000)
+
+    });
+}
+
+myPromise().then((res)=>{
+    console.log(res);
+    console.log(myPromise());
+}).catch((err)=>{
+    console.log("Error",err);
+    console.log(myPromise());
+})
