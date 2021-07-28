a5ebbfa587ded3a8ae3e0494eb6ad18f1e91c103
ehab qino <ehabqino@gmail.com>
2021-07-28 15:55:36 +0300

    commit "async await with more than promise "

--- async-await.js
+++ async-await.js
@@ -8,6 +8,7 @@ function myPromise(){
     });
 }
 
+/*
 async function getMyPromise(){
     try{
     const p = await myPromise();
@@ -16,5 +17,25 @@ async function getMyPromise(){
         console.log(err)
     }
 }
+*/
 
+function myPromise3(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(30);
+            //reject("Errrrrr");
+        },500)
+
+    });
+}
+
+async function getMyPromise(){
+    try{
+    const p1 = await myPromise();
+    const p2 = await myPromise3();
+    console.log(p1 , p2);
+    } catch(err) {
+        console.log(err)
+    }
+}
 getMyPromise();
