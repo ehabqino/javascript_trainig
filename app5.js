368319b7231512197d5a5997b14507218085ab1e
ehab qino <ehabqino@gmail.com>
2021-07-28 09:13:03 +0300

    commit "create card component"

---
+++ app5.js
@@ -0,0 +1,12 @@
+class CardComponent extends HTMLElement {
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+
+    }
+    disconnectedCallback(){
+
+    }
+}
+customElements.define('my-card',CardComponent);
