cdc3421a43101e9e5a38b26d83fcf6d18e33c388
ehab qino <ehabqino@gmail.com>
2021-07-28 09:57:08 +0300

    commit ""

--- app5.js
+++ app5.js
@@ -11,8 +11,12 @@ class CardComponent extends HTMLElement {
     render(){
         const cardTemplate = document.querySelector("#my-template");
         const cardTemplateContent = cardTemplate.content;
-
         this.shadowRoot.appendChild(cardTemplateContent.cloneNode(true));
+        
+        //check if the tag has attributes
+        if(this.hasAttribute("heading-color")){
+            this.shadowRoot.querySelector(".heading").getElementsByClassName.color = this.getAttribute('heading-color');
+        }
     }
 }
 customElements.define('my-card',CardComponent);
